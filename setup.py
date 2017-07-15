9cff26669ac7f32e7bb3e7cd453d7741ad41c8a6
Jacob Mastel <jacob@mastel.org>
2017-07-14 22:08:55 -0700

    added simple setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='pyanon'
+	, version='0.1'
+	, description='A package to help anonimize '
+	, url='http://github.com/storborg/funniest'
+	, author='Jacob Mastel'
+	, author_email='jacobm001@gmail.com'
+	, license='MIT'
+	, packages=['pyanon']
+	, zip_safe=False
+)
