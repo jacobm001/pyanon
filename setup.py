1dfec1bd681e3597d30ffe2820abb25dacc7ed91
Jacob Mastel <jacob@mastel.org>
2017-07-14 23:06:32 -0700

    updated description and url

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
 	name='pyanon'
 	, version='0.1'
-	, description='A package to help anonimize '
-	, url='http://github.com/storborg/funniest'
+	, description='A package to help anonimize data exports'
+	, url='https://github.com/jacobm001/pyanon'
 	, author='Jacob Mastel'
 	, author_email='jacobm001@gmail.com'
 	, license='MIT'
